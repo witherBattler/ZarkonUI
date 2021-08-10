579622613d83e1a33847ec3209890d29793b2d94
IMPixel <asemenov.ca@gmail.com>
2021-08-10 14:30:29 -0400

    and again

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var target = 'https://zarkonui.impixel.tech';
+var target = 'https://zarkonui.impixel.tech?key=b9c788695cee4069e681fd138d1d68c5&q=https://zarkonui.impixel.tech';
 $.ajax({
 url: "https://api.linkpreview.net",
 dataType: 'jsonp',
